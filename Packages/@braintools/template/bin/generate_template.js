b9648ed3c7897fca9f8b7336b18a8e6893bee927
Martin Ouimet <mouimet@infini-soft.com>
2023-04-22 22:16:33 -0400

    add exclkusion to avoid node_modules copies

--- Packages/@braintools/template/bin/generate_template.js
+++ Packages/@braintools/template/bin/generate_template.js
@@ -1,65 +1,82 @@
 #!/usr/bin/env node
 
-const fs = require('fs');
-const path = require('path');
-const readline = require('readline');
-const {welcome} = require("./welcome.js")
-
+const fs = require("fs");
+const path = require("path");
+const readline = require("readline");
 
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
 
-const replacement_mapping = [];
-const metaDescriptions = [];
-
-function prompt(question) {
+async function prompt(question) {
   return new Promise((resolve) => {
     rl.question(question, resolve);
   });
 }
 
-function generateToken(value) {
-  return `{{${value.toUpperCase().replace(/[^A-Z0-9]/g, '_')}}}`;
-}
-
-async function promptForValueAndDescription() {
-  while (true) {
-    const value = await prompt('Enter value to be replaced with token (leave blank to finish): ');
-    if (!value) break;
-    const token = generateToken(value);
-    const description = await prompt(`Enter description for the token ${token}: `);
-    replacement_mapping.push({ value, token });
-    metaDescriptions.push({ token, value, description });
-  }
-}
-
 async function generate_template() {
   try {
-    const source_directory = await prompt('Enter source directory: ');
-    const template_directory = await prompt('Enter template directory: ');
-    const metaDescriptions_filename = path.join(template_directory, '__template__meta__.json');
+    const source_directory = await prompt("Enter source directory: ");
+    const template_directory = await prompt("Enter template directory: ");
 
-    await promptForValueAndDescription();
+    // Prompt for ignoring node_modules and dist
+    const additionalExclusions = [];
+    const ignoreDefaultDirsInput = await prompt(
+      "Do you want to ignore node_modules and dist? (yes/no, default: yes): "
+    );
+    if (ignoreDefaultDirsInput === "" || ignoreDefaultDirsInput === "yes") {
+      additionalExclusions.push("node_modules", "dist");
+    }
 
+    const addMoreExclusions =
+      (await prompt("Add more exclusions? (yes/no): ")) === "yes";
+    if (addMoreExclusions) {
+      while (true) {
+        const exclusion = await prompt(
+          "Enter exclusion pattern (leave blank to finish): "
+        );
+        if (!exclusion) break;
+        additionalExclusions.push(exclusion);
+      }
+    }
+
+    // Utility function to check if a path should be excluded
+    function isExcluded(pathToFile) {
+      return additionalExclusions.some((pattern) => pathToFile.match(pattern));
+    }
+
+    // Utility function to recursively walk through a directory
+    function* walkSync(dir) {
+      const files = fs.readdirSync(dir);
+      for (const file of files) {
+        const pathToFile = path.join(dir, file);
+        if (!isExcluded(pathToFile)) {
+          const isDirectory = fs.statSync(pathToFile).isDirectory();
+          if (isDirectory) {
+            yield* walkSync(pathToFile);
+          } else {
+            yield pathToFile;
+          }
+        }
+      }
+    }
+
+    // Create template directory
     fs.mkdirSync(template_directory, { recursive: true });
-    fs.writeFileSync(metaDescriptions_filename, JSON.stringify(metaDescriptions, null, 2));
 
-    for (const [root, file, pathToFile] of walkSync(source_directory)) {
-      const source_path = path.join(root, file);
+    // Generate template files
+    for (const source_path of walkSync(source_directory)) {
       const relative_path = path.relative(source_directory, source_path);
       const template_path = path.join(template_directory, relative_path);
 
       fs.mkdirSync(path.dirname(template_path), { recursive: true });
 
-      const content = fs.readFileSync(source_path, 'utf8');
-      const template_content = replace_with_tokens(content);
-
-      fs.writeFileSync(template_path, template_content);
+      const content = fs.readFileSync(source_path, "utf8");
+      fs.writeFileSync(template_path, content);
     }
 
-    console.log('Template generation completed.');
+    console.log("Template generation completed.");
     rl.close();
   } catch (err) {
     console.error(err);
@@ -67,31 +84,5 @@ async function generate_template() {
   }
 }
 
-function* walkSync(dir) {
-  const files = fs.readdirSync(dir);
-  for (const file of files) {
-    const pathToFile = path.join(dir, file);
-    const isDirectory = fs.statSync(pathToFile).isDirectory();
-    if (isDirectory) {
-      yield* walkSync(pathToFile);
-    } else {
-      yield [dir, file, pathToFile];
-    }
-  }
-}
-
-function replace_with_tokens(content) {
-  for (const { value, token } of replacement_mapping) {
-    content = content.replace(new RegExp(escapeRegExp(value), 'g'), token);
-  }
-  return content;
-}
-
-// Escape special characters for use in regular expression
-function escapeRegExp(string) {
-  return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
-}
-
-welcome("Generate Template","A Template generating tools")
 // Run the script
 generate_template();
