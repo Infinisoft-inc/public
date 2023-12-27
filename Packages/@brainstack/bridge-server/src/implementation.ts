0de8c59eab08089cad6289824163dcc2e391b237
Martin Ouimet <mouimet@infinisoft.world>
2023-12-27 08:26:31 -0500

    workflow

--- Packages/@brainstack/bridge-server/src/implementation.ts
+++ Packages/@brainstack/bridge-server/src/implementation.ts
@@ -12,7 +12,7 @@ class BridgeServer {
   private uuidToSocket: Map<string, WebSocket>;
   private socketToUUID: Map<WebSocket, string>;
   public logger: Logger;
-  private hub:EventHub
+  private hub: EventHub;
 
   constructor(hub: EventHub, logger?: Logger) {
     this.port = PORT;
@@ -22,13 +22,12 @@ class BridgeServer {
     this.socketToUUID = new Map();
     this.wss = null;
     this.hub = hub;
-    hub.on(  /^(macro|meso)\.dts\.rawdata\.outgoing$/, this.broadcastToClients.bind(this));
   }
 
   private broadcastToClients(rawData: string): void {
     this.wss.clients.forEach((client) => {
       if (client.readyState === WebSocket.OPEN) {
-        client.send(rawData, (err) => {
+        client.send(JSON.stringify(rawData), (err) => {
           if (err) {
             this.logger.error('Error failed to broadcast to client:', err);
           }
@@ -39,21 +38,32 @@ class BridgeServer {
 
   public start(): void {
     this.setupListeners();
-    this.logger.info(`WebSocket server started on ws://${this.host}:${this.port}`);
+    this.logger.info(
+      `WebSocket server started on ws://${this.host}:${this.port}`
+    );
   }
 
   private setupListeners(): void {
     this.wss = new WebSocket.Server({ port: this.port, host: this.host });
 
     this.wss.on('connection', (ws: WebSocket) => {
+      this.hub.on(
+        /^(macro|meso)\.dts\.rawdata\.outgoing$/,
+        this.broadcastToClients.bind(this)
+      );
       const uuid = this.generateUUID();
       this.uuidToSocket.set(uuid, ws);
       this.socketToUUID.set(ws, uuid);
 
-      this.logger.verbose(`New client connection established with UUID: ${uuid}`);
+      this.logger.verbose(
+        `New client connection established with UUID: ${uuid}`
+      );
 
       ws.on('message', (rawData) => {
-        this.logger.verbose('Received raw data from client:', rawData.toString());
+        this.logger.verbose(
+          'Received raw data from client:',
+          rawData.toString()
+        );
         this.hub.emit('micro.websocket.rawdata.incoming', rawData);
       });
 
@@ -66,24 +76,30 @@ class BridgeServer {
       ws.on('error', (error) => {
         this.socketToUUID.delete(ws);
         this.uuidToSocket.delete(uuid);
-        this.logger.error(`Error on client connection with UUID: ${uuid}:`, error);
+        this.logger.error(
+          `Error on client connection with UUID: ${uuid}:`,
+          error
+        );
       });
     });
   }
 
   public gracefulShutdown() {
-    this.logger.info("Starting graceful shutdown of the BridgeServer.");
+    this.logger.info('Starting graceful shutdown of the BridgeServer.');
     this.wss.close(() => {
-      this.logger.info("All connections closed, shutting down server.");
+      this.logger.info('All connections closed, shutting down server.');
       process.exit(0);
     });
   }
 
   private generateUUID(): string {
-    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-      const r = (Math.random() * 16) | 0;
-      return (c == 'x' ? r : (r & 0x3 | 0x8)).toString(16);
-    });
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(
+      /[xy]/g,
+      function (c) {
+        const r = (Math.random() * 16) | 0;
+        return (c == 'x' ? r : (r & 0x3) | 0x8).toString(16);
+      }
+    );
   }
 }
 
