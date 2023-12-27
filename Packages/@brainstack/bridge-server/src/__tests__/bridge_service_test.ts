0dd18109e0dc71c375c03f57994c5ea6a9a1d731
Martin Ouimet <mouimet@infinisoft.world>
2023-12-27 08:58:13 -0500

    publish

--- Packages/@brainstack/bridge-server/src/__tests__/bridge_service_test.ts
+++ Packages/@brainstack/bridge-server/src/__tests__/bridge_service_test.ts
@@ -2,4 +2,4 @@ import { createEventHub } from "@brainstack/hub";
 import { BridgeServer } from "../implementation";
 import { createLogger } from "@brainstack/log";
 
-new BridgeServer(createEventHub(),createLogger(5))
+new BridgeServer(createLogger(5))
